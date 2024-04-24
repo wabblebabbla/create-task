2698bd3c994fdd6349f0c28fc8ab70fd626b2306
wabblebabbla <144172354+wabblebabbla@users.noreply.github.com>
2024-04-24 13:04:34 -0700

    Update task.py

--- task.py
+++ task.py
@@ -1,8 +1,9 @@
 symbol = "X"
-#create board
-board=[["-","-","-"],
-       ["-","-","-"],
-       ["-","-","-"]]
+#create board  
+board=[["-","-","-","-"], 
+       ["-","-","-","-"],
+       ["-","-","-","-"],
+       ["-","-","-","-"]]
 print(board)
 for i in range(9):
     choice = input("enter your move in the form RC")
@@ -14,19 +15,23 @@ for i in range(9):
         symbol = "0"
     else: 
         symbol = "X"
-    if board[0][0] == board[0][1] and board[0][1] == board[0][2]:
+    if board[0][0] == board[0][1] and board[0][1] == board[0][2] and board[0][2] == board[0][3]:
         print(symbol,"wins")
-    if board[1][0] == board[1][1] and board[1][1] == board[1][2]:
+    if board[1][0] == board[1][1] and board[1][1] == board[1][2] and board[1][2] == board[1][3]:
         print(symbol,"wins")
-    if board[2][0] == board[2][1] and board[2][1] == board[2][2]:
+    if board[2][0] == board[2][1] and board[2][1] == board[2][2] and board[2][2] == board[2][3]:
         print(symbol,"wins")
-    if board[0][0] == board[1][0] and board[1][0] == board[2][0]:
+    if board[3][0] == board[3][1] and board[3][1] == board[3][2] and board[3][2] == board[3][3]:
+        print(symbol,"wins")  
+    if board[0][0] == board[1][0] and board[1][0] == board[2][0] and board[2][0] == board[3][0]:
         print(symbol,"wins")
-    if board[0][1] == board[1][1] and board[1][1] == board[2][1]:
+    if board[0][1] == board[1][1] and board[1][1] == board[2][1] and board[2][1] == board[3][1]:
         print(symbol,"wins")
-    if board[0][2] == board[1][2] and board[1][2] == board[2][2]:
+    if board[0][2] == board[1][2] and board[1][2] == board[2][2] and board[2][2] == board[3][2]:
         print(symbol,"wins")
-    if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+    if board[0][3] == board[1][3] and board[1][3] == board[2][3] and board[2][3] == board[3][3]:
+        print(symbol,"wins")         
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[2][2] == board[3][3]:
         print(symbol,"wins")
-    if board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+    if board[3][0] == board[2][1] and board[2][1] == board[1][2] and board[1][2] == board[0][3]:
         print(symbol,"wins")
